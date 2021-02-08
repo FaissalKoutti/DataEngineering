00fa3f9541815cb548af4d9a9adc0ce6fb1114bd
benjaminnahum <74189101+benjaminnahum@users.noreply.github.com>
2021-02-08 02:46:37 +0100

    Update views.py

--- views.py
+++ views.py
@@ -28,7 +28,7 @@ es_client = Elasticsearch(hosts=["localhost" if ES_LOCAL else "elasticsearch"])
 df2 = pd.read_csv('Export_DataFrame.csv')
 df2 = df2.to_dict(orient='records')
 
-client = pymongo.MongoClient()
+client = pymongo.MongoClient('mongo')
 
 database = client['df2']
 
